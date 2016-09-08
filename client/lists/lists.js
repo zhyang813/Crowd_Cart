40e927aaa9b0851b98f19f294b9c305aa8994834
dan-h-ch <chang.daniel@gmail.com>
2016-09-08 06:49:48 -0700

    (feat) mylist - list detail in place

--- client/lists/lists.js
+++ client/lists/lists.js
@@ -36,6 +36,7 @@ angular.module("crowdcart.lists", [])
   $scope.displayDetail = function() {
     // store clicked data onto rootscope
     $rootScope.displayList = this.list
+    $location.path("/listdetail")
   }
 
   //TODO add new list method, will be attached into createnewlist.html
