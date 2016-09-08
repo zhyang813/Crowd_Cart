70c3fe8f881d9db3540938e2d1015ab5e5e8fdb0
Kyle Van Vleck <kylevanvleck@gmail.com>
2016-09-08 00:28:33 -0500

    added password encryption on userModel

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 var UserSchema = new mongoose.Schema({
   username: String,
@@ -16,5 +17,34 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
+UserSchema.methods.comparePasswords = function(enteredPassword, callback) {
+  var hashedPassword = this.password;
+  bcrypt.compare(enteredPassword, hashedPassword, function(err, match) {
+    callback(err, match);
+  })
+};
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, function(err, salt){
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash){
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      user.salt = salt;
+      next();
+    });
+  });
+});
 
 module.exports = mongoose.model('User', UserSchema);
