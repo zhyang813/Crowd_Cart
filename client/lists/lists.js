9aa563e632b5a36733ed1b8b1e12cbcdcd9e6d30
dan-h-ch <chang.daniel@gmail.com>
2016-09-12 11:16:43 -0700

    (fix) updated readme, enabled weekend dates

--- client/lists/lists.js
+++ client/lists/lists.js
@@ -224,7 +224,8 @@ angular.module("crowdcart.lists", ["angularMoment"])
   function disabled(data) {
     var date = data.date,
       mode = data.mode;
-    return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+    // remove disabled dates - prob could delete this entire section
+    return mode === 'day' && (date.getDay() === 10 || date.getDay() === 10);
   }
 
   $scope.toggleMin = function() {
